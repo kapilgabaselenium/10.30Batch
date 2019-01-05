86a1f70d81091ffe536e57934c6faf5a222fbf8f
Dell <Dell@DESKTOP-30FNJH5>
2019-01-04 21:23:20 -0800

    I want to push my code

---
+++ Jenkins_Project/src/pkg1/Class1.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+
+	@Test
+	public void tc1()
+	{
+		System.out.println("updated test case");
+	}
+	
+}
---
+++ Jenkins_Project/src/pkg1/Class2.java
@@ -0,0 +1,12 @@
+package pkg1;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+
+	@Test
+	public void tc1()
+	{
+		System.out.println("updated test case");
+	}
+}
